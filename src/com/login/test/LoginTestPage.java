ecbe69ef84e04627c45270e063b227ce4da0fa07
Sadashiv Byagi_ons <43012335+SadashivByagi@users.noreply.github.com>
2023-09-22 13:36:34 +0100

    Login class has been created

---
+++ src/com/login/test/LoginTestPage.java
@@ -0,0 +1,19 @@
+package com.login.test;
+
+public class LoginTestPage {
+	
+	public void LoginPage() {
+		System.out.println("This code is for login page");
+	}
+	
+	
+	public void Homepage() {
+		System.out.println("This code is for home page of our application");
+	}
+	
+	
+	public void Banking_module() {
+		System.out.println("This is access to Banking module in our application");
+	} 
+
+}
