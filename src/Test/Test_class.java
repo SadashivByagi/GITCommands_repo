9e00b28e712af36c79c5a547b156bbfa079801d0
Sadashiv Byagi_ons <43012335+SadashivByagi@users.noreply.github.com>
2023-09-22 13:27:12 +0100

    newly created class

---
+++ src/Test/Test_class.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class Test_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is test class");
+
+	}
+
+}
